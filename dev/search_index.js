1cf7477f5f499bb71f126f133d78302315f7943c
Documenter.jl <documenter@juliadocs.github.io>
2022-07-16 19:37:25 +0000

    build based on dee0553

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ZeroTrader","category":"page"},{"location":"#ZeroTrader","page":"Home","title":"ZeroTrader","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ZeroTrader.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ZeroTrader]","category":"page"}]
+}
